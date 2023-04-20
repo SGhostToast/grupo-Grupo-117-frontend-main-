809f41084a2f03c2eb7013a924bc3222f46d6f75
LilianBernot <lilian.bernot@gmail.com>
2023-04-20 18:19:36 -0400

    changing a bit of apparence of text. Adding mainpage content

---
+++ uno_online/src/mainpage/crear_partida.jsx
@@ -0,0 +1,13 @@
+export default function CrearPartida() {
+    return(
+        <>
+        <h1>Crear Partida</h1>
+        <p>Esta parte tendria un boton que permitiria crear una partida. 
+            El usuario llegara a una otra pagina que creera la partida rensenando el ID y preguntando de empezarl la partida cuando el creador quiere jugar !
+        </p>
+        <p>Existiria también un boton "Junta partida", llegando a una pagina preguntando por un ID de partida para juntar una. 
+            Quizas en esta otra pagina haria las partidas que esperan jugadores para entrar en partidas sin conocer el ID. 
+        </p>
+        </>
+    )
+}
