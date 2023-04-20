27d9643e4369d9a850ad4152e74c61e5bb089451
GhostToast <sarratiaa00@gmail.com>
2023-04-19 23:44:52 -0400

    initial style

--- uno_online/src/landing/landing.jsx
+++ uno_online/src/landing/landing.jsx
@@ -1,6 +1,11 @@
+import './styles/landing.css'
+
 export default function Landing() {
     return(
         <>
+        <div class="logo">
+            <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/UNO_Logo.svg/640px-UNO_Logo.svg.png"} alt="uno_logo"/>
+        </div>
         <h1>Bienvenidos</h1>
         </>
     )
