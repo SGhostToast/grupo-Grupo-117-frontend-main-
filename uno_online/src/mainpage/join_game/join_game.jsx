b15dcb36aababf0230cb41416db064039ee72abe
LilianBernot <lilian.bernot@gmail.com>
2023-04-21 18:25:27 -0400

    unirse a partida

--- uno_online/src/mainpage/join_game/join_game.jsx
+++ uno_online/src/mainpage/join_game/join_game.jsx
@@ -1,3 +1,5 @@
+import "./styles/join_game.css"
+
 export default function JoinGame() {
     return(
         <>
@@ -8,6 +10,35 @@ export default function JoinGame() {
             </div>
         </div>
         <div class="beneath">
+            <div class="partida">
+                <h3>Partida disponible</h3>
+                <p>Codigo partida : <span class="codigo">codigo</span></p>
+                <div class="unir_partida">
+                    <a href="/mainpage/join_game" >
+                        <boton class="unir_partida_boton">Unirme a partida</boton>
+                    </a>
+                </div>
+            </div>
+
+            <div class="partida">
+                <h3>Partida disponible</h3>
+                <p>Codigo partida : <span class="codigo">codigo</span></p>
+                <div class="unir_partida">
+                    <a href="/mainpage/join_game" >
+                        <boton class="unir_partida_boton">Unirme a partida</boton>
+                    </a>
+                </div>
+            </div> 
+            
+            <div class="partida">
+                <h3>Partida disponible</h3>
+                <p>Codigo partida : <span class="codigo">codigo</span></p>
+                <div class="unir_partida">
+                    <a href="/mainpage/join_game" >
+                        <boton class="unir_partida_boton">Unirme a partida</boton>
+                    </a>
+                </div>
+            </div>
         </div>
         </>
     )
