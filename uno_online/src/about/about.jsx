f8dc025b69c3e0dee09b38b55184f92f75405450
LilianBernot <lilian.bernot@gmail.com>
2023-04-21 11:08:50 -0400

    adding card content and function of navbar

--- uno_online/src/about/about.jsx
+++ uno_online/src/about/about.jsx
@@ -17,12 +17,17 @@ export default function About() {
                     <h2>Lilian</h2>
                 </div>
                 <div class="description">
-                <ul>
-                    
-                </ul>
-                <p>
-                  
-                </p>
+                    <ul>
+                        <li><strong>Pronombres:</strong> ilx/lx (il/le)</li>
+                        <li><strong>Generación:</strong> 2019</li>
+                        <li><strong>Major:</strong> Matematica y Fisica</li>
+                        <li><strong>Minor:</strong> Programación</li>
+                    </ul>
+                    <p>
+                        De momento en intercambio en Santiago de Chile, quiere aprovechar esta experiencia para mejorar su espanol y descubrir América del sur y sus pueblos.
+                        El ano necesita hacer una practica que, espera, sea en desarrollo web.
+                        Le gusta escalar !
+                    </p>
                 </div>
             </div>
             <br />
@@ -32,17 +37,17 @@ export default function About() {
                     <h2>Sam</h2>
                 </div>
                 <div class="description">
-                <ul>
-                    <li><strong>Pronombres:</strong> ellx/lx (elle/le)</li>
-                    <li><strong>Generación:</strong> 2019</li>
-                    <li><strong>Major:</strong> Robótica</li>
-                    <li><strong>Minor:</strong> Programación.</li>
-                </ul>
-                <p>
-                    Quiere dedicarse a crear videojuegos. Le gusta participar del arte en distintas
-                    formas, como en la música, el baile, las artes plásticas, etc.
-                    Se entretiene viendo animé, leyendo comics y jugando DnD. Le gusta mucho el té.
-                </p>
+                    <ul>
+                        <li><strong>Pronombres:</strong> ellx/lx (elle/le)</li>
+                        <li><strong>Generación:</strong> 2019</li>
+                        <li><strong>Major:</strong> Robótica</li>
+                        <li><strong>Minor:</strong> Programación</li>
+                    </ul>
+                    <p>
+                        Quiere dedicarse a crear videojuegos. Le gusta participar del arte en distintas
+                        formas, como en la música, el baile, las artes plásticas, etc.
+                        Se entretiene viendo animé, leyendo comics y jugando DnD. Le gusta mucho el té.
+                    </p>
                 </div>
             </div>
         </div>
