5bf6b7f43eff8f05572c44f4e733ea1b86d78220
LilianBernot <lilian.bernot@gmail.com>
2023-04-21 10:07:43 -0400

    intenté anadir dos columnas div pero me deja un horizontal scrolling...

--- uno_online/src/rules/tutorial.jsx
+++ uno_online/src/rules/tutorial.jsx
@@ -1,7 +1,20 @@
+import './styles/tutorial.css'
+
 export default function Tutorial() {
     return(
         <>
         <h1>Tutorial</h1>
+        {/* <div class='line'>
+            <div class='column'>
+                <p>oijqd oiAJQ DOIajq dpioal qzdjpo ialqzdsjxpoi LQSDJXIOP QLSDJ XOIQLS DJX OIQSLDJ XOiqslj dxoiQSL DJXOI QSLD</p>
+            </div>
+            <div class='column'>
+                <p>oijqd oiAJQ DOIajq dpioal qzdjpo ialqzdsjxpoi LQSDJXIOP QLSDJ XOIQLS DJX OIQSLDJ XOiqslj dxoiQSL DJXOI QSLD</p>
+            </div>
+        </div> */}
+
+{/* Don't understand why but does not work with two elements : bottom scrolling bar */}
+
         </>
     )
 }
