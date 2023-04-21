be57cd8f0bb81b253b2e86f7474de2a975f7f78b
GhostToast <sarratiaa00@gmail.com>
2023-04-20 20:15:09 -0400

    Estilo landig page, falta slogan

--- uno_online/src/landing/landing.jsx
+++ uno_online/src/landing/landing.jsx
@@ -3,10 +3,20 @@ import './styles/landing.css'
 export default function Landing() {
     return(
         <>
-        <div class="logo">
-            <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/UNO_Logo.svg/640px-UNO_Logo.svg.png"} alt="uno_logo"/>
+        <div id="title">
+            <img id="title-bg" src="https://st2.depositphotos.com/23387462/46229/i/600/depositphotos_462298254-stock-photo-minsk-belarus-march-2021-top.jpg" alt="" />
+            <div id="title-content">
+                <div id="logo">
+                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/UNO_Logo.svg/640px-UNO_Logo.svg.png"} alt="uno_logo"/>
+                </div>
+                <h1>¡Bienvenidxs a UNO Online!</h1>
+            </div>
         </div>
-        <h1>Bienvenidos</h1>
+        <div class="beneath">
+            <h2>Aquí va un slogan que aún no se me ocurre</h2>
+            <button id="login">Log In</button>
+            <button id="signin">Sign In</button>
+        </div>
         </>
     )
 }
